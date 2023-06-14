7c68a6866c6be5da09789df19e644d9c43df3693
Edria Taji <116660805+edris-taji@users.noreply.github.com>
2023-06-14 10:06:25 +0430

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Card());
+  runApp(MyApp());
 }
 
-class Card extends StatelessWidget {
-  const Card({super.key});
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const CircleAvatar(
-                  backgroundImage: AssetImage('images/edris.jpg'),
-                  radius: 50,
-                ),
-                const Text(
-                  'edris Taji',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontFamily: 'pacifico',
-                  ),
-                ),
-                const Text(
-                  'instructor at hemmat academy',
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontFamily: 'pacifico',
-                      letterSpacing: 2),
-                ),
-                Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/edris.jpg'),
+              ),
+              Text(
+                  'Edris Taji ',
+                      style: TextStyle(
+                        fontFamily: 'MTCORSVA',
+                       fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+              ),
+
+              ),
+              Text(
+                  'SOFWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'Mj_Nil 1',
                   color: Colors.white,
-                  padding: const EdgeInsets.all(5),
-                  margin:
-                  const EdgeInsets.symmetric(vertical: 25, horizontal: 55),
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('+93789062712')
-                    ],
-                  ),
+                  fontSize: 23,
+                  letterSpacing: 2.6,
+                  fontWeight: FontWeight.bold,
                 ),
-                Container(
+              ),
+              SizedBox(
+                height: 20,
+                width: 170,
+                child: Divider(
                   color: Colors.white,
-                  padding: const EdgeInsets.all(5),
-                  margin:
-                  const EdgeInsets.symmetric(vertical: 4, horizontal: 50),
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('edristaji256@gmail.com')
-                    ],
-                  ),
                 ),
-              ],
-            ),
+              ),
+              Card(
+               color: Colors.white,
+               // padding: EdgeInsets.all(10),
+               margin: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+               child: ListTile(
+                 leading: Icon(
+                   Icons.phone,
+                   color: Colors.lightBlue,
+                 ),
+                 title:Text(
+                   '+93 789023765',
+                   style: TextStyle(
+                     color: Colors.lightBlue,
+                     letterSpacing: 1.5,
+                     fontWeight: FontWeight.bold,
+                     fontSize: 15,
+                   ),
+                 ) ,
+               ),
+             ),
+              Card(
+                color: Colors.white,
+                // padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.lightBlue,
+                    ),
+                    title: Text(
+                      'edristaji256@gmail.com',
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        letterSpacing: 1.5,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
